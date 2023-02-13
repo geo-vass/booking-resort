c27ec78ad57a8c6f0191ac9e0aae40d27839bc25
George Vasiliou <georgevass4@gmail.com>
2023-02-13 08:22:48 +0200

    added NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { FaInstagram, FaFacebookF, FaTwitter, FaBars } from "react-icons/fa";
 import { MdClose } from "react-icons/md";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <section className="flex justify-between w-full px-4 py-2 min-h-[50px] items-center bg-gray-700/80 text-white">
       {/* Main nav */}
@@ -33,15 +36,20 @@ const NavBar = () => {
         </a>
       </div>
       {/* Hamburger Menu */}
-      <div size={20} className="sm:hidden cursor-pointer">
-        <FaBars />
+      <div size={20} className="md:hidden flex cursor-pointer">
+        <FaBars size={20} onClick={() => setNav(true)} />
       </div>
-      <div className="md:hidden  ease-in duration-300  text-2xl overflow-hidden absolute top-0 left-0 right-0 bottom-0  bg-black/90  text-gray-200 pt-10">
+      <div
+        className={`${
+          nav ? " top-0 left-0 " : " top-0 left-[-100%] "
+        }   md:hidden flex w-full h-full ease-in duration-500  justify-center text-2xl overflow-hidden absolute   bg-black/90  text-gray-200 pt-10`}
+      >
         <MdClose
           size={50}
-          className="text-white cursor-pointer absolute right-3 top-0 text-[var(--primary-dark)]"
+          className="text-white cursor-pointer absolute right-3 top-0 text-[var(--primary-dark)] ease-in duration-300"
+          onClick={() => setNav(false)}
         />
-        <ul className="flex flex-col items-center gap-10">
+        <ul className="flex flex-col items-center just gap-10">
           <li>
             <a href="/">Home</a>
           </li>
